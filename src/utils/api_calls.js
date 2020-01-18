e69694793a859253460b67c99ae611b197f03d84
Kevin Krato <kratok08@gmail.com>
2020-01-18 09:52:00 -0700

    Modify temperatures async fetch function to accept arguments dictating whether to fetch current weather or forecast data

--- src/utils/api_calls.js
+++ src/utils/api_calls.js
@@ -2,11 +2,12 @@ import { getUserCoordinates } from './helpers';
 require('dotenv').config();
 const url = 'http://api.openweathermap.org/data/2.5';
 
-export const fetchCurrentTemp = async () => {
+export const fetchTemperatures = async (format = 'weather') => {
+	// for 5-day-forecast set format arg to "forecast"
 	const { longitude, latitude } = await getUserCoordinates();
 	try {
 		let response = await fetch(
-			`${url}/weather?APPID=${process.env.REACT_APP_API_KEY}&lat=${latitude}&lon=${longitude}&units=imperial`
+			`${url}/${format}?APPID=${process.env.REACT_APP_API_KEY}&lat=${latitude}&lon=${longitude}&units=imperial`
 		);
 		return await response.json();
 	} catch (error) {
