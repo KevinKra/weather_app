140a1815933e3a1c4b2c0beecfbdc5453255cbc7
Kevin Krato <kratok08@gmail.com>
2020-01-18 09:42:32 -0700

    Update routes to include /forecast route, render Details component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Details from './pages/details/Details';
 
 function App() {
 	return (
 		<Router>
 			<Switch>
+				<Route path="/forecast">
+					<Details />
+				</Route>
 				<Route path="/">
 					<Home />
 				</Route>
