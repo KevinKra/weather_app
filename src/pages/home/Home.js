815161f3fb900ae6d32f7bebe3204ee50b530ea6
Kevin Krato <kratok08@gmail.com>
2020-01-18 09:52:53 -0700

    Integrate nav tag with Link to '/forecast' route

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import CurrentTemp from '../../components/CurrentTemp/CurrentTemp';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 	return (
 		<section>
+			<nav>
+				<Link to="/forecast">5-day Forecast</Link>
+			</nav>
 			<h3>Home Page</h3>
 			<CurrentTemp />
 		</section>
